94e9b4e7e66f9c2cb559012e00b6a2c835be0dd1
Cyrille YAO <cyaoyapi@gmail.com>
2020-09-21 13:04:37 +0000

    Django REST - Serializers
    Add models serializers for models Videos, Rating and User.
    
    Changes :
      - new file:   api/serializers.py

---
+++ app/api/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from .models import Video, Rating
+
+class VideoSerializer(serializers.ModelSerializer):
+    """Model serializer for model Video"""
+
+    class Meta:
+        model = Video
+        fields = '__all__'
+        extra_kwargs = {
+            'url': {
+                'required': True,
+            }
+        }
+
+class RatingSerializer(serializers.ModelSerializer):
+    """Model serializer for model Rating"""
+
+    class Meta:
+        model = Rating
+        fields = ('id', 'video', 'user', 'starts', 'comments',)
+
+class UserSerializer(serializers.ModelSerializer):
+    """Model serializer for model User"""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {
+            'password': {
+                'required': True,
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        """Override default create method
+
+        For each user, create and link a token to it.
+        This token will be required to use the api.
+        """
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
