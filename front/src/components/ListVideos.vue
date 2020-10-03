9e55ffe864cec3ff9e33676b93b499ad8b5952ac
Cyrille YAO <cyaoyapi@gmail.com>
2020-10-03 18:36:45 +0000

    Vue v-for looping
    Changes :
    
      - modified:   front/src/components/ListVideos.vue

--- front/src/components/ListVideos.vue
+++ front/src/components/ListVideos.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="">
     <div class="row">
-      <div class="col-md-5 text-center">
+      <div class="col-md-5 text-center nicefont">
             <h1>Welcome to youtube videos rating</h1>
-            <button @click="getVideos">Get videos</button>
+            <hr>
+            <div v-for="video in videos" :key="video.id">
+              <h4>{{ video.title }}</h4>
+              <p>Rating : {{ video.ratings_average }}</p>
+              <button class="btn-sm btn-primary mt-2 mb-3">Details</button>
+            </div>
       </div>
     </div>
   </div>
@@ -22,14 +27,21 @@ export default {
   methods: {
     getVideos() {
       axios.get('http://127.0.0.1:8000/api/videos/')
-      .then(res => (console.log(res.data)))
+      .then(res => (this.videos = res.data))
       .catch(err => console.log(err)) ;
     }
+  },
+  created(){
+    this.getVideos() ;
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Mukta:wght@300&display=swap');
 
+.nicefont {
+  font-family: 'Mukta', sans-serif;
+}
 </style>
